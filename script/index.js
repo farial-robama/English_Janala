9f577112aef5cea2a1ecc89ff50dbd8103789cc6
farial-robama <farialrobama15@gmail.com>
2025-09-05 13:48:57 +0200

    add spinner and display words by lesson

--- script/index.js
+++ script/index.js
@@ -1,3 +1,14 @@
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
+
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res)=> res.json())
@@ -7,6 +18,34 @@ const loadLessons = () => {
         })
 
 };
+const loadLevelWord = (id) => {
+  manageSpinner(true);
+  fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+      <h2>${word.word}</h2>
+      <p>${word.meaning} / ${word.pronunciation}</p>
+      <button onclick="loadWordDetail(${word.id})">Details</button>
+      <button onclick="pronounceWord('${word.word}')">🔊</button>
+    `;
+    wordContainer.append(card);
+  });
+  manageSpinner(false);
+};
+
+
+
+
 const displayLesson = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
