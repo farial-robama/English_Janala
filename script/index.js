182aee4832219a982bffa2cb0cbb19ff7dfbc5d6
farial-robama <farialrobama15@gmail.com>
2025-09-05 10:47:40 +0200

    fetch and display lesson buttons from API

--- script/index.js
+++ script/index.js
@@ -1,9 +1,23 @@
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res)=> res.json())
-    .then((json) => console.log(json.data));
+    .then((json) => {
+            console.log(json.data);
+            displayLesson(json.data);       
+        })
+
 };
 const displayLesson = (lessons) => {
-    
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML = "";
+    for (const lesson of lessons) {
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-outline btn-primary lesson-btn">
+        <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+        </button>
+        `;
+        levelContainer.append(btnDiv)
+    }
 }
 loadLessons()
